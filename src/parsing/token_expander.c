c23b1775800334299be485dbb60c2472c6c697d3
almeekel <almeekel@student.42lyon.fr>
2025-07-29 17:54:07 +0200

    normed source material in parsing and utils

--- src/parsing/token_expander.c
+++ src/parsing/token_expander.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   token_expander.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nagaudey <nagaudey@student.42.fr>          +#+  +:+       +#+        */
+/*   By: almeekel <almeekel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 18:11:15 by almeekel          #+#    #+#             */
-/*   Updated: 2025/07/24 14:35:17 by nagaudey         ###   ########.fr       */
+/*   Updated: 2025/07/29 17:53:28 by almeekel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,39 +27,20 @@ int	should_field_split(t_quote quote_type)
 
 t_token	*expand_tokens(t_token *tokens, char **envp, int exit_status)
 {
-	t_token	*current;
-	t_token	*expanded_head;
-	t_token	*prev;
-	char	*op_copy;
+	t_token				*current;
+	t_token				*expanded_head;
+	t_token				*prev;
+	t_expand_context	ctx;
 
+	ctx.envp = envp;
+	ctx.exit_status = exit_status;
 	expanded_head = NULL;
 	current = tokens;
 	prev = NULL;
 	while (current)
 	{
-		if (current->type == T_WORD)
-		{
-			if (prev && prev->type == T_HEREDOC)
-			{
-				op_copy = ft_strdup(current->value);
-				if (!op_copy || !create_and_append_token(&expanded_head,
-						op_copy, current->type, current->quote))
-					return (free_token_list_and_return_null(expanded_head));
-			}
-			else
-			{
-				if (!process_word_expansion(current, &expanded_head, envp,
-						exit_status))
-					return (free_token_list_and_return_null(expanded_head));
-			}
-		}
-		else
-		{
-			op_copy = ft_strdup(current->value);
-			if (!op_copy || !create_and_append_token(&expanded_head, op_copy,
-					current->type, Q_NONE))
-				return (free_token_list_and_return_null(expanded_head));
-		}
+		if (!process_single_token(current, prev, &expanded_head, &ctx))
+			return (free_token_list_and_return_null(expanded_head));
 		prev = current;
 		current = current->next;
 	}
--- src/utils/quote_stripper.c
+++ src/utils/quote_stripper.c
@@ -3,57 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   quote_stripper.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nagaudey <nagaudey@student.42.fr>          +#+  +:+       +#+        */
+/*   By: almeekel <almeekel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 13:49:12 by almeekel          #+#    #+#             */
-/*   Updated: 2025/07/24 14:34:47 by nagaudey         ###   ########.fr       */
+/*   Updated: 2025/07/29 17:41:02 by almeekel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 #include "utils.h"
 
+static int	handle_variable_expansion(t_str_builder *sb, const char **ip,
+		char **envp)
+{
+	if (should_expand_variable(**ip, *(*ip + 1), Q_DOUBLE)
+		|| should_expand_variable(**ip, *(*ip + 1), Q_NONE))
+	{
+		if (!process_expansion(sb, ip, envp, 0))
+			return (0);
+		return (1);
+	}
+	return (0);
+}
+
 static int	process_heredoc_char(t_str_builder *sb, const char **ip,
 		char **envp, char *current_quote)
 {
 	if (!*current_quote && (**ip == '\'' || **ip == '"'))
-	{
-		*current_quote = **ip;
-		if (!sb_append_char(sb, **ip))
-			return (0);
-		(*ip)++;
-	}
+		return (handle_quote_start(sb, ip, current_quote));
 	else if (*current_quote && **ip == *current_quote)
-	{
-		if (!sb_append_char(sb, **ip))
-			return (0);
-		*current_quote = 0;
-		(*ip)++;
-	}
+		return (handle_quote_end(sb, ip, current_quote));
 	else if (*current_quote == '\'')
-	{
-		if (!sb_append_char(sb, **ip))
-			return (0);
-		(*ip)++;
-	}
+		return (handle_single_quote(sb, ip));
 	else if (*current_quote == '"' && **ip == '\\' && (*(*ip + 1) == '$'
 			|| *(*ip + 1) == '"' || *(*ip + 1) == '\\' || *(*ip + 1) == '\n'))
-	{
-		(*ip)++;
-		if (!sb_append_char(sb, **ip))
-			return (0);
-		(*ip)++;
-	}
-	else if (should_expand_variable(**ip, *(*ip + 1), Q_DOUBLE))
-	{
-		if (!process_expansion(sb, ip, envp, 0))
-			return (0);
-	}
-	else if (should_expand_variable(**ip, *(*ip + 1), Q_NONE))
-	{
-		if (!process_expansion(sb, ip, envp, 0))
-			return (0);
-	}
+		return (handle_double_quote_escape(sb, ip));
+	else if (handle_variable_expansion(sb, ip, envp))
+		return (1);
 	else
 	{
 		if (!sb_append_char(sb, **ip))
