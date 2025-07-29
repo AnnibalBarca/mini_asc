b73b289636d30844cc0e0d1d122e31ca1b880c8a
almeekel <almeekel@student.42lyon.fr>
2025-07-29 17:34:54 +0200

    normed env managers

--- src/utils/variable_handler.c
+++ src/utils/variable_handler.c
@@ -3,64 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   variable_handler.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nagaudey <nagaudey@student.42.fr>          +#+  +:+       +#+        */
+/*   By: almeekel <almeekel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 11:37:24 by almeekel          #+#    #+#             */
-/*   Updated: 2025/07/28 15:10:54 by nagaudey         ###   ########.fr       */
+/*   Updated: 2025/07/29 17:18:18 by almeekel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/utils.h"
-
-static int	replace_env_var(char ***env_ptr, int index, char *name, char *value)
-{
-	char	*new_var_string;
-	char	*old_var;
-
-	new_var_string = create_env_string(name, value);
-	if (!new_var_string)
-		return (1);
-	old_var = (*env_ptr)[index];
-	(*env_ptr)[index] = new_var_string;
-	free(old_var);
-	return (0);
-}
-
-static int	add_new_env_var(char ***env_ptr, char *name, char *value, int i)
-{
-	char	**new_env;
-	char	*new_var_string;
-	int		count;
-
-	new_var_string = create_env_string(name, value);
-	if (!new_var_string)
-		return (1);
-	count = count_env_vars(*env_ptr);
-	new_env = malloc(sizeof(char *) * (count + 2));
-	if (!new_env)
-	{
-		free(new_var_string);
-		return (1);
-	}
-	while (i < count)
-	{
-		new_env[i] = ft_strdup((*env_ptr)[i]);
-		if (!new_env[i])
-		{
-			while (--i >= 0)
-				free(new_env[i]);
-			free(new_env);
-			free(new_var_string);
-			return (1);
-		}
-		i++;
-	}
-	new_env[count] = new_var_string;
-	new_env[count + 1] = NULL;
-	ft_freesplit(*env_ptr);
-	*env_ptr = new_env;
-	return (0);
-}
 
 char	*find_env_value(char **envp, const char *name)
 {
@@ -74,19 +24,6 @@ char	*find_env_value(char **envp, const char *name)
 	if (!equals_pos)
 		return (NULL);
 	return (equals_pos + 1);
-}
-
-int	set_env_var(char ***env_ptr, char *name, char *value)
-{
-	int	var_index;
-
-	if (!env_ptr || !*env_ptr || !name)
-		return (1);
-	var_index = find_env_index(*env_ptr, name);
-	if (var_index != -1)
-		return (replace_env_var(env_ptr, var_index, name, value));
-	else
-		return (add_new_env_var(env_ptr, name, value, 0));
 }
 
 void	remove_env_variable(char ***env_ptr, const char *name)
